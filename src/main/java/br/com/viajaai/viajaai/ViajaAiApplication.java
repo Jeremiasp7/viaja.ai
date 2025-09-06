c71696eba870aad6a2764eb3ca9220a551b42ec0
Jeremiasp7 <jeremiasp2004@gmail.com>
2025-09-06 15:17:46 -0300

    first commit

---
+++ src/main/java/br/com/viajaai/viajaai/ViajaAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.viajaai.viajaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viajaai/viajaai/ViajaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viajaai.viajaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
