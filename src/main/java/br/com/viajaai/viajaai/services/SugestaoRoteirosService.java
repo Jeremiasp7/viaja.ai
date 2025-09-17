1ad9fc50ca59224c80046df4c625a3405e88b0f5
Jeremiasp7 <jeremiasp2004@gmail.com>
2025-09-16 21:05:07 -0300

    mudanças no modelo e no prompt

--- src/main/java/br/com/viajaai/viajaai/services/SugestaoRoteirosService.java
+++ src/main/java/br/com/viajaai/viajaai/services/SugestaoRoteirosService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import br.com.viajaai.viajaai.entities.UserEntity;
 import br.com.viajaai.viajaai.entities.UsersPreferencesEntity;
 import br.com.viajaai.viajaai.repositories.UserRepository;
-
 @Service
 public class SugestaoRoteirosService {
     
@@ -27,7 +26,7 @@ public class SugestaoRoteirosService {
         UsersPreferencesEntity preferencias = user.getPreferences();
 
         String textoDePreferencias = """
-                Para a minha viagem, atualmente prefiro um estilo %s e tem uma preferência por ficar hospedado em %s. À respeito do clima, prefiro locais com o clima %s, ou que pelo menos algo próximo disso. Financeiramente, tenho disponível %s para fazer essa viagem, que será, preferencialmente de %s dias. Irei para essa viagem com %s. Por fim, tenho as seguintes datas disponível para a viagem: %s.
+                O usuário quer uma viagem %s, acomodado em um %s, em uma cidade com o clima %s. Ele possui %s para gastar com a viagem. Vai viajar por %s dias com %s. Ele pode viajar em uma das seguintes datas %s.
                 """.formatted(
                     preferencias.getEstiloDeViagem(),
                     preferencias.getPreferenciaDeAcomodacao(),
@@ -39,12 +38,11 @@ public class SugestaoRoteirosService {
                 );
 
         String prompt = """
-                Gere uma sugestão de roteiro de viagens básico, baseado nas preferências do usuário descritas abaixo do usuário, %s.
+                Haja como se você fosse uma agência brasileira de viagens de excelência e sugira um roteiro de viagens para um cliente com base nas preferências abaixo: 
                 
                 %s.
-
-                Baseado no texto acima, quero que você monte um roteiro de viagens que seja coerente com o que o usuário informou. O roteiro de viagens deve conter: Uma localização, sugestões de atrações turísticas nessa localização, o que o usuário e as companhias (caso ele não vá sozinho) podem fazer nos dias em que ele estiver viajando. No roteiro, deve informar quais deverão ser as atividades do usuário por dia. Lembrando que a quantidade MÁXIMA de dias é a quantidade de dias que ele informou. Faça o roteiro.
-                """.formatted(user.getNome(), textoDePreferencias);
+                
+                """.formatted(textoDePreferencias);
 
         return chatClient.prompt()
                 .user(prompt)
