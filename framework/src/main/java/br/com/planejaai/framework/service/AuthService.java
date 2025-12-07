daa18a2dad211c62c6da2b04670c9c53d2d637d4
Jose Eduardo Monteiro <mds.josee@gmail.com>
2025-12-07 16:46:15 -0300

    fix small errors

--- framework/src/main/java/br/com/planejaai/framework/service/AuthService.java
+++ framework/src/main/java/br/com/planejaai/framework/service/AuthService.java
@@ -1,7 +1,10 @@
 package br.com.planejaai.framework.service;
 
+import br.com.planejaai.framework.dto.LoginRequestDto;
 import br.com.planejaai.framework.dto.LoginResponseDto;
 import br.com.planejaai.framework.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
 import java.time.Duration;
 import java.time.Instant;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +15,21 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
   private final JwtEncoder jwtEncoder;
   private final UserRepository userRepository;
   private final UserService userService;
 
-  public ResponseEntity<LoginResponseDto> login(LoginResponseDto login) {
+  public ResponseEntity<LoginResponseDto> login(LoginRequestDto loginRequest) {
     var user = userRepository.findByEmail(loginRequest.getEmail());
 
     if (user.isEmpty() || !userService.isLoginCorrect(loginRequest, user.get())) {
       throw new BadCredentialsException("user or password is invalid!");
     }
 
-    var expiresIn = Duration.buildByDays(1).getMilliseconds();
+    var expiresIn = Duration.ofDays(1).toMillis();
 
     var now = Instant.now();
     var claims =
